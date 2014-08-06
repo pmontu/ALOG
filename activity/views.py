0bce2e50c31df24c9e50042b190892d2db50ff72
Manoj <pmontu@gmail.com>
2014-08-06 16:30:54 +0530

    atempt - transpose data from two tables

--- activity/views.py
+++ activity/views.py
@@ -9,7 +9,17 @@ from activity.models import Datehour
 from datetime import date, timedelta
 
 def index(request):
-	activity = "Hello"
+	activities_last_5_days = []
 
-	context = {'activity': activity}
+	i=5
+	while i>0:
+		day = Datehour.objects.filter(date=date.today()-timedelta(days=i))
+		Activities_day = Activity.objects.filter(datehour=day)
+		activities_last_5_days.append(Activities_day)
+		i-=1
+
+	activities_last_5_days = zip(*activities_last_5_days)
+
+
+	context = {'activities_last_5_days': activities_last_5_days}
 	return render(request, 'activity/index.html', context)
